82a6413c343de6f952fa9d98a22e649453595830
torresed <torrese@ufl.edu>
2021-01-25 16:51:47 -0500

    Remove Devise rememberable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :validatable
 
   validates :username, presence: true, length: { minimum: 5, maximum: 20 }
 end
