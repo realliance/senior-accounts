847275f04facba8a458fbcadb79f73e6917184ec
torresed <torrese@ufl.edu>
2021-01-28 20:31:55 -0500

    Add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :auth_token
 
-  validates :email, presence: true, uniqueness: true, length: 1..100
+  validates :email, presence: true, uniqueness: true, length: 1..100, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, presence: true, uniqueness: true, length: 3..100
   validates :password, length: 10..72, allow_blank: true
   validates :auth_token, uniqueness: true
