e201a1f9bbf36452cf3bb2e56230f339ff3ea4d8
torresed <torrese@ufl.edu>
2021-02-03 02:27:42 -0500

    Add password recovery endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   post :session, to: 'users#create_session'
   delete :session, to: 'users#destroy_session'
   get '/confirm/:email_confirmation_token', to: 'users#confirm_email', as: 'confirm_email'
+  post '/password/recovery', to: 'users#password_recovery'
+  get '/password/:password_recovery_token', to: 'users#password_reset', as: 'password_reset'
 end
