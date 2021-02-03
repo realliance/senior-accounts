df03d47e1014790c04641e2c4ce7db8164d3558e
ConnorJC <ccatlett2000@mctherealm.net>
2021-02-02 23:41:29 -0500

    Fix sentry violations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,14 @@ class ApplicationController < ActionController::API
   private
 
   def check_token
-    @current_user = authenticate_or_request_with_http_token do |token|
+    @current_user = authenticate_with_http_token do |token|
       User.find_by(auth_token: token)
     end
-    Sentry.set_user(id: @user.id, email: @user.email, username: @user.username)
+    if @current_user.nil?
+      request_http_token_authentication
+    else
+      Sentry.set_user(id: @current_user.id, username: @current_user.username, email: @current_user.email)
+    end
   end
 
   attr_reader :current_user
