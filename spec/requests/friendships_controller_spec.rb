78a78426ac107e7761730ba5a8b3f01803163d89
torresed <torrese@ufl.edu>
2021-02-06 22:55:49 -0500

    Add unit tests

---
+++ spec/requests/friendships_controller_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+# RSpec.describe FriendshipsController, type: :request do
+# context 'when logged in' do
+# describe 'POST #friendship' do
+# let(:user) do
+# create(:user)
+# end
+
+# let(:valid_headers) do
+#  { authorization: "Token #{user.auth_token}" }
+# end
+
+# it 'sends a friend request' do
+# post friendship_url, params: { id: user.id }, headers: valid_headers, as: :json
+#  expect(friendship).to include(friendship.user)
+# end
+# end
+# end
+# end
