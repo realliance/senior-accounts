a73edcf113a64af7f5ed6dc2f6df2b2796d9dd2f
torresed <torrese@ufl.edu>
2021-02-13 04:27:31 -0500

    Dropped friendships id false pege

--- db/migrate/20210206031504_create_friendships.rb
+++ db/migrate/20210206031504_create_friendships.rb
@@ -2,7 +2,7 @@
 
 class CreateFriendships < ActiveRecord::Migration[6.1]
   def change
-    create_table :friendships do |t|
+    create_table :friendships, id: false do |t|
       t.references :sent_by, foreign_key: { to_table: :users }, type: 'blob(20)'
       t.references :sent_to, foreign_key: { to_table: :users }, type: 'blob(20)'
       t.column :status, :integer
