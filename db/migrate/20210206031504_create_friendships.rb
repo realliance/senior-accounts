6f21ff203f908edd24bc13d17ac0b7e83a06d2e7
torresed <torrese@ufl.edu>
2021-02-13 04:19:13 -0500

    Remove friendships primary key

--- db/migrate/20210206031504_create_friendships.rb
+++ db/migrate/20210206031504_create_friendships.rb
@@ -2,9 +2,7 @@
 
 class CreateFriendships < ActiveRecord::Migration[6.1]
   def change
-    create_table :friendships, id: false do |t|
-      t.ksuid_binary :id, primary_key: true
-
+    create_table :friendships do |t|
       t.references :sent_by, foreign_key: { to_table: :users }, type: 'blob(20)'
       t.references :sent_to, foreign_key: { to_table: :users }, type: 'blob(20)'
       t.column :status, :integer
