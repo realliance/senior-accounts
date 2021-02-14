49b9dc8370c4340c6578ff6cbaf3166b2b3efcc6
torresed <torrese@ufl.edu>
2021-02-14 13:46:19 -0500

    Fixup DB PK

--- db/migrate/20210206031504_create_friendships.rb
+++ db/migrate/20210206031504_create_friendships.rb
@@ -3,6 +3,8 @@
 class CreateFriendships < ActiveRecord::Migration[6.1]
   def change
     create_table :friendships, id: false do |t|
+      t.ksuid_binary :id, primary_key: true
+
       t.references :sent_by, foreign_key: { to_table: :users }, type: 'blob(20)'
       t.references :sent_to, foreign_key: { to_table: :users }, type: 'blob(20)'
       t.column :status, :integer
