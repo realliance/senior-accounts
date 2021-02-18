b0e4f56ff5cddb644e120ac709df23b1b1f7c169
torresed <torrese@ufl.edu>
2021-02-17 19:38:38 -0500

    Add invite, accept_invite, kick, disband

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -9,7 +9,7 @@ class FriendsController < ApplicationController
   end
 
   def create
-    @friendship = Friends.request(@user, @friend)
+    @friendship = Friends.request(current_user, @friend)
     if @friendship.nil?
       render status: :bad_request, json: { error: 'Friend request could not be sent.' }
     else
@@ -35,6 +35,42 @@ class FriendsController < ApplicationController
       @friendship.destroy
       render status: :ok, json: { success: 'Friend has been removed.' }
     end
+  end
+
+  def invite
+    @friendship = Friends.find_by(sent_by: current_user, sent_to: @friend, status: :confirmed)
+    if @friendship.nil? || @friendship.invited? || current_user.party_members.count >= 2
+      render status: :bad_request, json: { error: 'Friend could not be invited.' }
+    else
+      @friendship.invited!
+      render status: :ok, json: { success: 'Friend has been invited.' }
+    end
+  end
+
+  def accept_invitation
+    @friendship = Friends.find_by(sent_by: @friend, sent_to: current_user, status: :confirmed)
+    if @friendship.nil? || !@friendship.invited? || @friend.party_members.count >= 2
+      render status: :bad_request, json: { error: 'Invitation could not be accepted.' }
+    else
+      @friendship.accepted!
+      render status: :ok, json: { success: 'Invitation accepted.' }
+    end
+  end
+
+  def kick
+    @friendship = Friends.find_by(sent_by: current_user, sent_to: @friend, status: :confirmed)
+    if @friendship.nil? || !@friendship.accepted?
+      render status: :bad_request, json: { error: 'Friend could not be kicked.' }
+    else
+      @friendship.unsolicited!
+      render status: :ok, json: { success: 'Friend has been kicked.' }
+    end
+  end
+
+  def disband
+    current_user.party_members.destroy
+    current_user.invitations.destroy
+    render status: :ok, json: { success: 'Party has been disbanded.' }
   end
 
   private
