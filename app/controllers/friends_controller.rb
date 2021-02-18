37835f610c37f827eb741f75319b81d5e5d1a481
torresed <torrese@ufl.edu>
2021-02-17 20:23:49 -0500

    Add block user feature

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -10,7 +10,7 @@ class FriendsController < ApplicationController
 
   def create
     @friendship = Friends.request(current_user, @friend)
-    if @friendship.nil?
+    if @friendship.nil? || @friendship.blocked?
       render status: :bad_request, json: { error: 'Friend request could not be sent.' }
     else
       render status: :ok, json: { success: 'Friend request has been sent.' }
@@ -18,7 +18,7 @@ class FriendsController < ApplicationController
   end
 
   def update
-    @friendship = Friends.find_by(sent_by: @friend, sent_to: current_user, status: 'pending')
+    @friendship = Friends.find_by(sent_by: @friend, sent_to: current_user, status: :pending)
     if @friendship.nil?
       render status: :bad_request, json: { error: 'Friend request could not be accepted.' }
     else
@@ -37,40 +37,14 @@ class FriendsController < ApplicationController
     end
   end
 
-  def invite
-    @friendship = Friends.find_by(sent_by: current_user, sent_to: @friend, status: :confirmed)
-    if @friendship.nil? || @friendship.invited? || current_user.party_members.count >= 2
-      render status: :bad_request, json: { error: 'Friend could not be invited.' }
+  def block
+    @friendship = Friends.find_by(sent_by: @friend, sent_to: current_user).presence || Friends.find_by(sent_by: current_user, sent_to: @friend).presence
+    if @friendship.nil?
+      render status: :bad_request, json: { error: 'User could not be blocked.' }
     else
-      @friendship.invited!
-      render status: :ok, json: { success: 'Friend has been invited.' }
+      @friendship.blocked!
+      render status: :ok, json: { success: 'User has been blocked.' }
     end
-  end
-
-  def accept_invitation
-    @friendship = Friends.find_by(sent_by: @friend, sent_to: current_user, status: :confirmed)
-    if @friendship.nil? || !@friendship.invited? || @friend.party_members.count >= 2
-      render status: :bad_request, json: { error: 'Invitation could not be accepted.' }
-    else
-      @friendship.accepted!
-      render status: :ok, json: { success: 'Invitation accepted.' }
-    end
-  end
-
-  def kick
-    @friendship = Friends.find_by(sent_by: current_user, sent_to: @friend, status: :confirmed)
-    if @friendship.nil? || !@friendship.accepted?
-      render status: :bad_request, json: { error: 'Friend could not be kicked.' }
-    else
-      @friendship.unsolicited!
-      render status: :ok, json: { success: 'Friend has been kicked.' }
-    end
-  end
-
-  def disband
-    current_user.party_members.destroy
-    current_user.invitations.destroy
-    render status: :ok, json: { success: 'Party has been disbanded.' }
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@
 
 Rails.application.routes.draw do
   resource :user, only: %i[create update show]
+
   post :session, to: 'users#create_session'
   delete :session, to: 'users#destroy_session'
+
   get '/confirm/:email_confirmation_token', to: 'users#confirm_email', as: 'confirm_email'
   post '/password/recovery', to: 'users#password_recovery'
   get '/password/reset/:password_recovery_token', to: 'users#password_reset', as: 'password_reset'
   post '/password/reset/:password_recovery_token', to: 'users#password_update', as: 'password_update'
+
   resource :friends, only: %i[show create update destroy]
+  post '/friends/block', to: 'friends#block'
+
   post '/friends/invite', to: 'friends#invite'
   post '/friends/accept_invitation', to: 'friends#accept_invitation'
   post '/friends/kick', to: 'friends#kick'
